b5ccc5c0288697bb3f777c0d4b17ea5650722b50
Tony McGuckin <tony.mcguckin@ie.ibm.com>
2026-01-07 20:52:33 +0000

    Update unit test

--- utils.js
+++ utils.js
@@ -1,7 +1,7 @@
 
 
   
-const welcomeMessage = "Welcome to IBM Cloud DevOps using Containers. Let\'s go use the Continuous Delivery Service";
+const welcomeMessage = "Welcome to IBM Cloud DevOps using Code Engine and Github Actions!";
 const portMessage = "Application Running on port";
 
 const getWelcomeMessage = () => {
